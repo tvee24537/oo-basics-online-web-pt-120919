00625ab633a26d0fa7b4026ef236440017eb38a0
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-01-10 05:07:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :brand
+  attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   def initialize(brand)
-    @brand
+    @brand = brand
   end
 end
